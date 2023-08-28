5897bdedfe6d272f261216ad23e6569c3ed72e04
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-08-28 12:14:53 +0200

    update

--- portfolio-gallery/src/portfolio-gallery/Gallery.js
+++ portfolio-gallery/src/portfolio-gallery/Gallery.js
@@ -12,16 +12,16 @@ function Gallery() {
     const tabsData = ["all", ...new Set(categoryData)];
 
     const filterCategory = (category) => {
-        if (category == "all") {
+        if (category === "all") {
             setData(Data);
             return;
         }
         const filteredData = Data.filter((value) => {
-            return value.category == category;
+            return value.category === category;
         })
         setData(filteredData);
     }
-    
+
     return (
 
         <div className="container">
--- portfolio-gallery/src/portfolio-gallery/Items.js
+++ portfolio-gallery/src/portfolio-gallery/Items.js
@@ -7,7 +7,7 @@ function Items({ data }) {
                     return (
                         <div className="col-sm-3 my-3" key={id}>
                             <div className="card bg-light text-center">
-                                <img src={image} className="img-fluid" style={{ height: "200px" }} />
+                                <img src={image} alt="language" className="img-fluid" style={{ height: "200px" }} />
                                 <h3>{title}</h3>
                                 <p>{description}</p>
                             </div>
